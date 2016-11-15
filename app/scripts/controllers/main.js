c28fbe60a4fcb947b24c2fa80449944eb66baae6
richyst <ricardo_1994@hotmail.com>
2016-11-14 21:16:01 -0600

    Scaffolding de Yeoman con Bower arreglado por errores de generator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name larryFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the larryFrontApp
+ */
+angular.module('larryFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
