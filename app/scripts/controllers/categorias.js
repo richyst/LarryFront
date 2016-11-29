4bb5e330f580cce0873a44f7fae5280c7c0d587c
richyst <ricardo_1994@hotmail.com>
2016-11-28 22:20:38 -0600

    avance del día

--- app/scripts/controllers/categorias.js
+++ app/scripts/controllers/categorias.js
@@ -12,63 +12,68 @@ angular.module('larryFrontApp')
     $scope.term =Number($routeParams.id);
     $scope.preguntas=[];
     $scope.datos=$window.sessionStorage;
-    $http.get('http://localhost:8080/Preguntas').success(function(data) {
-        $scope.preguntas1 = data;
-        angular.forEach($scope.preguntas1, function(item){
-          item.resps=0;
-          item.fecha= item.fecha.slice(0,10);
-          if(item.idCat===$scope.term){
-            $scope.preguntas.push(item);
-          }
-        });
-        $http.get('http://localhost:8080/Users').success(function(data) {
-            $scope.users = data;
-            angular.forEach($scope.preguntas, function(preg){
-              angular.forEach($scope.users, function(user){
-                if(preg.idUser===user.id){
-                  preg.campus=user.campus;
-                  preg.username=user.username;
+    $scope.editPreg=null;
+    $scope.infoTotal = function (){
+      $scope.preguntas=[];
+      $http.get('http://localhost:8080/Preguntas').success(function(data) {
+          $scope.preguntas1 = data;
+          angular.forEach($scope.preguntas1, function(item){
+            item.resps=0;
+            item.fecha= item.fecha.slice(0,10);
+            if(item.idCat===$scope.term){
+              $scope.preguntas.push(item);
+            }
+          });
+          $http.get('http://localhost:8080/Users').success(function(data) {
+              $scope.users = data;
+              angular.forEach($scope.preguntas, function(preg){
+                angular.forEach($scope.users, function(user){
+                  if(preg.idUser===user.id){
+                    preg.campus=user.campus;
+                    preg.username=user.username;
+                  }
+                });
+              });
+          }).error(function(data){
+            console.log("Error en get de json");
+          });
+          $http.get('http://localhost:8080/Respuestas').success(function(data) {
+              $scope.respuestas = data;
+              angular.forEach($scope.preguntas, function(preg){
+                angular.forEach($scope.respuestas, function(resp){
+                  if(preg.id===resp.idPreg){
+                    if(resp.res===true){
+                      preg.status=true;
+                      preg.check="preguntaRes";
+                    }
+                    if(resp.res===false && preg.status!==true){
+                      preg.status=false;
+                      preg.check="pregunta";
+                    }
+                    preg.resps++;
+                  }
+                });
+                if(preg.resps===0){
+                  preg.status=false;
+                  preg.check="pregunta";
                 }
               });
-            });
-        }).error(function(data){
-          console.log("Error en get de json");
-        });
-        $http.get('http://localhost:8080/Respuestas').success(function(data) {
-            $scope.respuestas = data;
-            angular.forEach($scope.preguntas, function(preg){
-              angular.forEach($scope.respuestas, function(resp){
-                if(preg.id===resp.idPreg){
-                  if(resp.res===true){
-                    preg.status=true;
-                    preg.check="preguntaRes";
-                  }
-                  if(resp.res===false && preg.status!==true){
-                    preg.status=false;
-                    preg.check="pregunta";
-                  }
-                  preg.resps++;
-                }
-              });
-              if(preg.resps===0){
-                preg.status=false;
-                preg.check="pregunta";
-              }
-            });
-        }).error(function(data){
-          console.log("Error en get de json");
-        });
-        $http.get('http://localhost:8080/Categorias/'+$scope.term).success(function(data) {
-            $scope.categoria = data;
-        }).error(function(data){
-          console.log("Error en get de json");
-        });
+          }).error(function(data){
+            console.log("Error en get de json");
+          });
+          $http.get('http://localhost:8080/Categorias/'+$scope.term).success(function(data) {
+              $scope.categoria = data;
+          }).error(function(data){
+            console.log("Error en get de json");
+          });
 
 
-    }).error(function(data){
-      console.log("Error en get de json");
-    });
+      }).error(function(data){
+        console.log("Error en get de json");
+      });
+    };
 
+    $scope.infoTotal();
     $scope.login=function(){
       $http.post('http://localhost:8080/api/login', {
             username: $scope.userReg.username,
@@ -80,6 +85,7 @@ angular.module('larryFrontApp')
             $scope.obtenerDatosUser();
         });
     };
+
     $scope.logout=function(){
       $window.sessionStorage.clear();
       $scope.datos=$window.sessionStorage;
@@ -105,9 +111,28 @@ angular.module('larryFrontApp')
             texto:newPreg.texto,
             titulo:newPreg.titulo,
             idUser:$scope.datos.userId
-        }).then(function (response) {
-            console.log(response);
+        }).then(function () {
+            $scope.infoTotal();
         });
+    };
+    $scope.borrarPregunta = function(preg){
+      $http.delete('http://localhost:8080/Preguntas/'+preg.id, {
+
+        }).then(function () {
+            $scope.infoTotal();
+        });
+    };
+    $scope.editarPregunta = function(preg){
+      $http.put('http://localhost:8080/Preguntas/'+preg.id, {
+            idCat:$scope.categoria.id,
+            texto:preg.texto,
+            titulo:preg.titulo
+        }).then(function () {
+            $scope.infoTotal();
+        });
+    };
+    $scope.selPregunta = function(preg){
+      $scope.editPreg=preg;
     };
 
     $scope.navegar = function(id){
--- app/scripts/controllers/preguntas.js
+++ app/scripts/controllers/preguntas.js
@@ -10,66 +10,70 @@
 angular.module('larryFrontApp')
   .controller('PreguntasCtrl', function ($scope, $http, $location, $routeParams, $window) {
     $scope.term =Number($routeParams.id);
-    $http.get('http://localhost:8080/Preguntas/'+$scope.term).success(function(data) {
-        $scope.pregunta = data;
-        $scope.pregunta.resps=0;
-        $scope.respuestas=[];
-        $scope.datos=$window.sessionStorage;
-        $scope.pregunta.fecha= $scope.pregunta.fecha.slice(0,10);
-        $http.get('http://localhost:8080/Respuestas').success(function(data) {
-            $scope.respuestas1 = data;
-            angular.forEach($scope.respuestas1, function(resp){
-              if($scope.pregunta.id===resp.idPreg){
-                resp.votado=0;
-                $scope.respuestas.push(resp);
-                if(resp.res===true){
-                  $scope.pregunta.status=true;
+    $scope.infoTotal=function(){
+      $http.get('http://localhost:8080/Preguntas/'+$scope.term).success(function(data) {
+          $scope.pregunta = data;
+          $scope.pregunta.resps=0;
+          $scope.respuestas=[];
+          $scope.datos=$window.sessionStorage;
+          $scope.pregunta.fecha= $scope.pregunta.fecha.slice(0,10);
+          $http.get('http://localhost:8080/Respuestas').success(function(data) {
+              $scope.respuestas1 = data;
+              angular.forEach($scope.respuestas1, function(resp){
+                if($scope.pregunta.id===resp.idPreg){
+                  resp.votado=0;
+                  $scope.respuestas.push(resp);
+                  if(resp.res===true){
+                    $scope.pregunta.status=true;
+                  }
+                  if(resp.res===false && $scope.pregunta.status!==true){
+                    $scope.pregunta.status=false;
+                  }
+                  $scope.pregunta.resps++;
                 }
-                if(resp.res===false && $scope.pregunta.status!==true){
-                  $scope.pregunta.status=false;
-                }
-                $scope.pregunta.resps++;
-              }
-            });
-            $http.get('http://localhost:8080/Users').success(function(data) {
-                $scope.users = data;
-                angular.forEach($scope.respuestas, function(resp){
-                  angular.forEach($scope.users, function(user){
-                    if(resp.username===user.username){
-                      resp.user=user;
-                    }
+              });
+              $http.get('http://localhost:8080/Users').success(function(data) {
+                  $scope.users = data;
+                  angular.forEach($scope.respuestas, function(resp){
+                    angular.forEach($scope.users, function(user){
+                      if(resp.username===user.username){
+                        resp.user=user;
+                      }
+                    });
                   });
-                });
 
-            }).error(function(data){
-              console.log("Error en get de json");
-            });
-            if($scope.pregunta.resps===0){
-              $scope.pregunta.status=false;
-              $scope.pregunta.check="pregunta";
-            }
+              }).error(function(data){
+                console.log("Error en get de json");
+              });
+              if($scope.pregunta.resps===0){
+                $scope.pregunta.status=false;
+                $scope.pregunta.check="pregunta";
+              }
 
-        }).error(function(data){
-          console.log("Error en get de json");
-        });
-        $http.get('http://localhost:8080/Users/'+$scope.pregunta.idUser).success(function(data) {
-            $scope.user = data;
-            $scope.pregunta.campus=$scope.user.campus;
-            $scope.pregunta.username=$scope.user.username;
+          }).error(function(data){
+            console.log("Error en get de json");
+          });
+          $http.get('http://localhost:8080/Users/'+$scope.pregunta.idUser).success(function(data) {
+              $scope.user = data;
+              $scope.pregunta.campus=$scope.user.campus;
+              $scope.pregunta.username=$scope.user.username;
 
-        }).error(function(data){
-          console.log("Error en get de json");
-        });
-        $http.get('http://localhost:8080/Categorias/'+$scope.pregunta.idCat).success(function(data) {
-            $scope.categoria = data;
-        }).error(function(data){
-          console.log("Error en get de json");
-        });
+          }).error(function(data){
+            console.log("Error en get de json");
+          });
+          $http.get('http://localhost:8080/Categorias/'+$scope.pregunta.idCat).success(function(data) {
+              $scope.categoria = data;
+          }).error(function(data){
+            console.log("Error en get de json");
+          });
 
 
-    }).error(function(data){
-      console.log("Error en get de json");
-    });
+      }).error(function(data){
+        console.log("Error en get de json");
+      });
+    };
+    $scope.infoTotal();
+
     $scope.login=function(){
       $http.post('http://localhost:8080/api/login', {
             username: $scope.userReg.username,
@@ -104,11 +108,20 @@ angular.module('larryFrontApp')
       resp.score=(resp.score+valor);
       $http.put('http://localhost:8080/Respuestas/'+resp.id,{
         score:resp.score
-      }).then(function(){
-        console.log(resp.score);
+      }).then(function(response){
+        console.log(response);
       });
     };
     $scope.nuevaRespuesta = function(resp){
-      console.log(resp + $scope.term);
+      $http.post('http://localhost:8080/Respuestas', {
+            idPreg: $scope.pregunta.id,
+            res:false,
+            score:0,
+            texto:resp,
+            username:$scope.datos.userReg
+        }).then(function (response) {
+          console.log(response);
+          $scope.infoTotal();
+        });
     };
   });
